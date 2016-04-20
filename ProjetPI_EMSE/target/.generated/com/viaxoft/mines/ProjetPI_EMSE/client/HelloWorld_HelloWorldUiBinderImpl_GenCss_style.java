3f9a26fdf058ff826b9f95161d47f5abe1659405
pomme-abricot <gaetan.heidsieck@gmail.com>
2016-04-20 16:29:50 +0200

    suppression de target

--- ProjetPI_EMSE/target/.generated/com/viaxoft/mines/ProjetPI_EMSE/client/HelloWorld_HelloWorldUiBinderImpl_GenCss_style.java
+++
@@ -1,7 +0,0 @@
-package com.viaxoft.mines.ProjetPI_EMSE.client;
-
-import com.google.gwt.resources.client.CssResource;
-
-public interface HelloWorld_HelloWorldUiBinderImpl_GenCss_style extends CssResource {
-  String important();
-}
--- ProjetPI_EMSE/target/.generated/com/viaxoft/mines/ProjetPI_EMSE/client/application/ApplicationView_BinderImpl_GenCss_style.java
+++
@@ -1,8 +0,0 @@
-package com.viaxoft.mines.ProjetPI_EMSE.client.application;
-
-import com.google.gwt.resources.client.CssResource;
-
-public interface ApplicationView_BinderImpl_GenCss_style extends CssResource {
-  String labels();
-  String buttons();
-}
